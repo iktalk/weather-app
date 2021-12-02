9f19f9801a04d9e91848b4d84808ca70e0b03ff7
iktalk <iktalk.94@gmail.com>
2021-12-01 20:17:23 -0500

    Update main icon image alt

--- src/script.js
+++ src/script.js
@@ -57,6 +57,7 @@ function showTemperature(response) {
     "src",
     `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
   );
+  iconElement.setAttribute("alt", response.data.weather[0].description);
 }
 
 function searchCity(city) {
